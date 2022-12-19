4970594d7026cd88ca26c29cf092dfcb582d284f
nadiiaTykhoniuk <matsonka02@gmail.com>
2022-12-19 13:51:21 +0200

    feat: create project

---
+++ app/Jobs/Example1/KillRussian.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs\Example1;
+
+use App\Models\Russian;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class KillRussian implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $russian;
+    public $uniqueFor = 360;
+    public $delay = 10;
+    public $retryAfter = 5;
+    public $tries = 5;
+    public $maxExceptions = 3;
+
+    public function __construct(Russian $russian)
+    {
+        $this->russian = $russian;
+    }
+
+    public function handle()
+    {
+        $this->russian->kill();
+    }
+
+    public function uniqueId()
+    {
+        return $this->russian->id;
+    }
+
+    public function retryAfter()
+    {
+        return 5;
+    }
+
+    public function retryUntil()
+    {
+        return now()->addMinutes(10);
+    }
+}
---
+++ app/Models/Russian.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Russian extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'is_dead'
+    ];
+
+    public function kill()
+    {
+        $this->update([
+            'is_dead' => true
+        ]);
+    }
+}
