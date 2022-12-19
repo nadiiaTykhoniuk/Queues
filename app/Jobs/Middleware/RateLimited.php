e4ef03a936a1826d713a9cca878bfc6ceb3ab59d
ananasik02 <matsonka02@gmail.com>
2022-12-19 14:45:00 +0200

    feat(middleware): create rate limiting middlewar

---
+++ app/Jobs/Middleware/RateLimited.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs\Middleware;
+
+use Illuminate\Support\Facades\Cache;
+
+class RateLimited
+{
+    public function handle($job, $next) {
+
+        // We need some identifier for a group of jobs
+        // In case we want to apply the same cache lock for all jobs,
+        // set the same group to all jobs
+        $jobGroup = $job->getJobGroup();
+
+        // Create a cache lock for 5 seconds
+        $lock = Cache::lock($jobGroup, 5);
+
+        // Trying to get a lock and fire a job (if 5 seconds passed)
+        if ($lock->get()) {
+            return $next($job);
+        }
+
+        // Send a job back to the queue if the lock can't acquired
+        return $job->release();
+    }
+}
